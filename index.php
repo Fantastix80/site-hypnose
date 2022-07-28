d122747516e0a501b2e775160c4f6790fa3f3d84
Fantastix80 <jean.vanwittenberge@gmail.com>
2022-07-28 22:04:05 +0200

    ttt

---
+++ index.php
@@ -0,0 +1,32 @@
+<!-- Intégration du header -->
+<?php require_once "views/layouts/header.php" ?>
+
+<!-- Header -->
+<header>
+
+    <div class="user">
+        <p><a href="#">S'inscrire</a> | <a href="#">Se connecter</a></p>
+    </div>
+
+    <section class="navbar">
+        <div class="logo">
+            <img src="assets/img/logo-header.png" alt="logo nicole westerhoek">
+        </div>
+        <nav>
+            <ul>
+                <li><a href="index.php">Accueil</a></li>
+                <li><a href="#">Sophrologie</a></li>
+                <li><a href="#">Hypnose</a></li>
+                <li><a href="#">Tarifs</a></li>
+            </ul>
+            <ul>
+                <li><a href="#">Contactez-moi !</a></li>
+            </ul>
+        </nav>
+    </section>
+
+</header>
+
+
+<!-- Intégration du footer -->
+<?php require_once "views/layouts/footer.php" ?>
